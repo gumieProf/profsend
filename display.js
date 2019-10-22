367efdebb15fc91f274078ccc2e110b1fddddcde
guts <mine>
2019-10-22 15:50:38 +0900

    FIX html

--- display.js
+++
@@ -1,3 +0,0 @@
-    var ua = navigator.userAgent.toLowerCase();
-    if(0 < ua.indexOf("iphone") || 0 < ua.indexOf("android") && 0 < ua.indexOf("mobile")) { ($("body").width("360px");}
-    if(0 < ua.indexOf("iPad") || 0 < ua.indexOf("Android")) { $("body").width("1024px");}
