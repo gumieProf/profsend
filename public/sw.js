23937cbb53d2396c900dd1e9491b406e41416dba
gutsmine <mine>
2020-01-15 19:37:38 +0900

    FIX html swjs ADD css js

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,26 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'profsend-pwa-v1';
 var urlsToCache = [
-    'https://profsend.herokuapp.com/',
+    'top.html',
     'css/style.css',
-    
+    'css/bootstrap-4.3.1.css',
+    'image/card-img.png',
+    'image/en_prof.png',
+    'image/favicon.ico',
+    'image/image.png',
+    'image/img-1.png',
+    'image/ja.prof.png',
+    'image/prof.png',
+    'image/toku.png',
+    'script/download.js',
+    'script/jquery.js',
+    'script/main.js',
+    'css/materialize.css',
+    'js/materialize.js',
+    'css/all.css',
+    'about.html',
+    '13update.html',
+    '404.html'
 ];
 
 // インストール処理
