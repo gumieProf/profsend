2e1150ba81335b86fd0647a8a477a54778034e09
gutsmine <mine>
2020-01-21 16:33:28 +0900

    FIX sub.js

--- public/script/sub.js
+++ public/script/sub.js
@@ -2,12 +2,10 @@ $(function() {
   $("#snsDataBtn").click(function() {
     var div = $("#snsData");
     var elems =
-      '<div class="input-field col s12">'+
-
-      '<input type="text" name="formSNS" id="sns" class="wrap"  maxlength="25" onkeyup="document.getElementById('sns-leng').value=this.value.length" placeholder="">'+
-      '<label for="sns">SNS情報：</label>'+
-      '</div>'+
-
+      '<div class="input-field col s12">' +
+      '<input type="text" name="formSNS" id="sns" class="wrap"  maxlength="25" onkeyup="document.getElementById("sns-leng").value=this.value.length" placeholder="">' +
+      '<label for="sns">SNS情報：</label>' +
+      "</div>" +
       '<div class="input-field col s12">' +
       "<br />" +
       '<input type="text" id="sns-leng" readonly></input>' +
