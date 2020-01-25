673b982bc3468abd87b7efaa8e658136afb538cf
gutsmine <mine>
2020-01-25 14:21:20 +0900

    ADD appjs bundlejs webpack FIX tophtml package

---
+++ public/script/app.js
@@ -0,0 +1,7 @@
+import main from "./main";
+import sub from "./sub";
+import download from "./download";
+
+var Main = new main();
+var Sub = new sub();
+var dl = new download();
--- public/script/sub.js
+++ public/script/sub.js
@@ -1,21 +1,19 @@
-$(function() {
-  $("#snsDataBtn").click(function() {
-    var div = $("#snsData");
-    var elems =
-      '<div class="input-field col s12">' +
-      '<input type="text" name="formSNS" id="sns" class="wrap"  maxlength="25" onkeyup="document.getElementById("sns-leng").value=this.value.length" placeholder="">' +
-      '<label for="sns">SNS情報：</label>' +
-      "</div>" +
-      '<div class="input-field col s12">' +
-      "<br />" +
-      '<input type="text" id="sns-leng" readonly></input>' +
-      '<label for="genzainomojisuB">現在の文字数</label>' +
-      "</div>" +
-      "</div>";
-    if (document.getElementById("sns") != null) {
-      div.empty();
-    } else {
-      div.html(elems);
-    }
+export default function sub() {
+  $(function() {
+    $("#snsDataBtn").click(function() {
+      var div = $("#snsData");
+      var elems =
+        '<div class="input-field col s12">' +
+        '<input type="text" name="formSNS" id="snsText" class="profelem"  maxlength="25" placeholder="">' +
+        '<label for="snsText">SNS情報：</label>' +
+        "</div>";
+      if (document.getElementById("snsText") != null) {
+        div.empty();
+      } else {
+        div.html(elems);
+      }
+      $(document).on("keyup", "#snsText", i);
+      $(document).on("change", "#snsText", i);
+    });
   });
-});
+}
