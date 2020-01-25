704c3b45587b566bbe1a1501b2b76ecb0dabbc51
gutsmine <mine>
2020-01-25 14:21:20 +0900

    ADD appjs bundlejs webpack FIX tophtml package

--- public/script/main.js
+++ public/script/main.js
@@ -1,87 +1,151 @@
-﻿$(document).ready(function() {
-    var o = 35
-      , c = 1
-      , r = 30
-      , u = 30
-      , prof = document.getElementById("prof")
-      , m = prof.getContext("2d");
-    (new Image).src = "" + pic;
+﻿export default function main() {
+  $(function() {
+    var o = 35,
+      c = 1,
+      r = 30,
+      u = 30,
+      prof = document.getElementById("prof"),
+      m = prof.getContext("2d");
+    new Image().src = "" + pic;
     var e = document.getElementById("file");
-    m.fillStyle = "" + color,
-    m.fillRect(0, 0, 450, 300),
-    m.fillStyle = "" + text,
-    m.rect(15, 15, 370, 170),
-    m.lineWidth = 8,
-    m.stroke();
-    $("#name").val(),
-    $("#id").val(),
-    $("#age").val(),
-    $("#coment").val();
-    function i() {
-        var age =    $("#age").val();
-        var coment = $("#coment").val();
-        if(age == null || age == undefined || age == "" && coment == null || coment == undefined || coment == ""){
-        var e = "ユーザー名:\t" + $("#name").val()+ "\nID: \t" + $("#id").val();
-        } else if(age == null || age == undefined || age == ""){
-        var e = "ユーザー名:\t" + $("#name").val() + "\nID: \t" + $("#id").val() + "\nコメント: \t" + $("#coment").val();
-        } else if(coment == null || coment == undefined || coment == ""){
-        var e = "ユーザー名:\t" + $("#name").val() + "\nID: \t" + $("#id").val() + "\n 年齢: \t" + $("#age").val();
-        } else {
-        var e = "ユーザー名:\t" + $("#name").val()+ "\nID: \t" + $("#id").val() + "\n 年齢: \t" + $("#age").val() + "\nコメント: \t" + $("#coment").val();
-        }
-        
-        m.clearRect(0, 0, 500, 300);
-        m.beginPath();
-        m.fillStyle = "" + color;
-        m.fillRect(0, 0, 500, 300);
-        m.fillStyle = "" + text;
-        m.rect(15, 15, 370, 170);
-        m.lineWidth = 8;
-        m.stroke();
-var fontSize = 24 ;	// フォントサイズ
-var lineHeight = 1.1618 ;	// 行の高さ (フォントサイズに対する倍率)
-var x = 50 ;	// 水平位置
-var y = 50 ;	// 垂直位置
-        m.font = "bold " + o + "Noto Sans JP";
-for( var lines=e.split( "\n" ), i=0, l=lines.length; l>i; i++ ) {
-	var line = lines[i] ;
-	var addY = fontSize ;
+    var i = function() {
+      var age = $("#age").val();
+      var coment = $("#coment").val();
+      var sns = $("#snsText").val();
+      var comentif =
+        coment == null || typeof coment == "undefined" || coment == "";
+      var snsif = sns == null || typeof sns == "undefined" || sns == "";
+      var ageif = age == null || typeof age == "undefined" || age == "";
 
-	// 2行目以降の水平位置は行数とlineHeightを考慮する
-	if ( i ) addY += fontSize * lineHeight * i ;
+      if (comentif && snsif && ageif) {
+        var e =
+          "ユーザー名:\t" + $("#name").val() + "\nID: \t" + $("#id").val();
+      } else if (comentif && snsif) {
+        var e =
+          "ユーザー名:\t" +
+          $("#name").val() +
+          "\nID: \t" +
+          $("#id").val() +
+          "\n 年齢: \t" +
+          age;
+      } else if (ageif && snsif) {
+        var e =
+          "ユーザー名:\t" +
+          $("#name").val() +
+          "\nID: \t" +
+          $("#id").val() +
+          "\nコメント: \t" +
+          $("#coment").val();
+      } else if (ageif && comentif) {
+        var e =
+          "ユーザー名:\t" +
+          $("#name").val() +
+          "\nID: \t" +
+          $("#id").val() +
+          "\nSNS: \t" +
+          $("#snsText").val();
+      } else if (ageif) {
+        var e =
+          "ユーザー名:\t" +
+          $("#name").val() +
+          "\nID: \t" +
+          $("#id").val() +
+          "\nコメント: \t" +
+          $("#coment").val() +
+          "\nSNS: \t" +
+          $("#snsText").val();
+      } else if (comentif) {
+        var e =
+          "ユーザー名:\t" +
+          $("#name").val() +
+          "\nID: \t" +
+          $("#id").val() +
+          "\n 年齢: \t" +
+          age +
+          "\nSNS: \t" +
+          $("#snsText").val();
+      } else if (snsif) {
+        var e =
+          "ユーザー名:\t" +
+          $("#name").val() +
+          "\nID: \t" +
+          $("#id").val() +
+          "\n 年齢: \t" +
+          age +
+          "\nコメント: \t" +
+          $("#coment").val();
+      } else {
+        var e =
+          "ユーザー名:\t" +
+          $("#name").val() +
+          "\nID: \t" +
+          $("#id").val() +
+          "\n 年齢: \t" +
+          age +
+          "\nコメント: \t" +
+          $("#coment").val() +
+          "\nSNS: \t" +
+          $("#snsText").val();
+      }
 
-	m.fillText( line, x + 0, y + addY ) 
+      m.clearRect(0, 0, 500, 300);
+      m.beginPath();
+      m.fillStyle = "" + color;
+      m.fillRect(0, 0, 500, 300);
+      m.fillStyle = "" + text;
+      m.rect(15, 15, 370, 170);
+      m.lineWidth = 8;
+      m.stroke();
+      var fontSize = 24; // フォントサイズ
+      var lineHeight = 1.1618; // 行の高さ (フォントサイズに対する倍率)
+      var x = 50; // 水平位置
+      var y = 30; // 垂直位置
+      m.font = "bold " + o + "YuGothic";
+      for (var lines = e.split("\n"), i = 0, l = lines.length; l > i; i++) {
+        var line = lines[i];
+        var addY = fontSize;
 
-               ;
-                var a = new Image;
-                a.src = "" + pic,
-                m.drawImage(a, 200, 70, 100, 100)
-         }
-        }
-    i(),
-    $("#name, #id, #age, #coment").on("keyup", i);
-    $("#name, #id, #age, #coment").on("change", i);
+        // 2行目以降の水平位置は行数とlineHeightを考慮する
+        if (i) addY += fontSize * lineHeight * i;
+
+        m.fillText(line, x + 0, y + addY);
+        var a = new Image();
+        (a.src = "" + pic), m.drawImage(a, 200, 70, 100, 100);
+      }
+    };
+    window.i = i;
+    (m.fillStyle = "" + color),
+      m.fillRect(0, 0, 450, 300),
+      (m.fillStyle = "" + text),
+      m.rect(15, 15, 370, 170),
+      (m.lineWidth = 8),
+      m.stroke();
+    $("#name").val(), $("#id").val(), $("#age").val(), $("#coment").val();
+    $("#snsText").val();
+    i(), $(".profelem").keyup(i);
+    $(".profelem").change(i);
     $("#colors").change(function() {
-        color = $(this).val(),
-        i()
+      (color = $(this).val()), i();
     }),
-    $("#idset").click(function() {
+      $("#idset").click(function() {
         var e = document.getElementById("idtype").value;
-        $("#idtext").html('<span>ID：</span><input type="text" name="formID" id="id" class="wrap" maxlength="' + e + '"></input>'),
-        i(),
-        $("#idleng").html("<h5>現在の桁数は" + e + "です</h5>")
-    }),
-    e.addEventListener("change", function(e) {
+        $("#idtext").html(
+          '<span>ID：</span><input type="text" name="formID" id="id" class="wrap" maxlength="' +
+            e +
+            '"></input>'
+        ),
+          i(),
+          $("#idleng").html("<h5>現在の桁数は" + e + "です</h5>");
+      }),
+      e.addEventListener("change", function(e) {
         var t = e.target.files[0];
         if (t.type.match("image.*")) {
-            var n = new FileReader;
-            n.onload = function() {
-                pic = n.result,
-                i()
-            }
-            ,
-            n.readAsDataURL(t)
-        } else
-            alert("画像を選択してください")
-    });
-});
+          var n = new FileReader();
+          (n.onload = function() {
+            (pic = n.result), i();
+          }),
+            n.readAsDataURL(t);
+        } else alert("画像を選択してください");
+      });
+  });
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require("path");
+const BomPlugin = require("webpack-utf8-bom");
+const webpack = require("webpack");
+const jquery = require("jquery");
+
+module.exports = {
+  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+  mode: "development",
+  // エントリーポイントの設定
+  entry: "./public/script/app.js",
+  // 出力の設定
+  output: {
+    // 出力するファイル名
+    filename: "bundle.js",
+    // 出力先のパス（絶対パスを指定する必要がある）
+    path: path.join(__dirname, "public/script/")
+  },
+  plugins: [
+    new BomPlugin(true),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ]
+};
