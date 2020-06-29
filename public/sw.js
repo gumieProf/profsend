bf3084e3609033c8ca2f52d179c5e2261b4d4f32
gumieProf <mine>
2020-06-29 18:44:48 +0900

    FIX html ejs css js(READ LICENSE)

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2020 gumie
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 var CACHE_NAME = "profsend-pwa-v1";
 var urlsToCache = [
   "top.pwa.html",
@@ -41,10 +65,10 @@ self.addEventListener("install", function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(
         urlsToCache.map(
-          (url) => new Request(url, { credentials: "same-origin" })
-        )
+          (url) => new Request(url, { credentials: "same-origin" }),
+        ),
       );
-    })
+    }),
   );
 });
 
@@ -52,6 +76,6 @@ self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response ? response : fetch(event.request);
-    })
+    }),
   );
 });
