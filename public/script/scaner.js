f171e44d27bfbb2b8aa9c51fb67c6c4c22ec4cd7
gumieProf <mine>
2020-07-08 19:12:18 +0900

    FIX swjs scaner.js

--- public/script/scaner.js
+++ public/script/scaner.js
@@ -12,15 +12,14 @@ $(function () {
           console.log(m);
           $("#loading").text("実行中...")
         }
-      }
-        .then(function (result) {
-          var matchrs = result.match(/[ID:]+[0-9a-zA-Z]{9,25}/);
-          matchrs.slice(5);
-          $("#loading").text("実行完了！");
-          console.log(matchrs);
-          $("#scanData").text(matchrs).select();
-          document.execCommand("copy");
-        })
-    )
+      })
+      .then(function (result) {
+        var matchrs = result.text.match(/[ID:]+[0-9a-zA-Z]{9,25}/);
+        matchrs.slice(5);
+        $("#loading").text("実行完了！");
+        console.log(matchrs);
+        $("#scanData").text(matchrs).select();
+        document.execCommand("copy");
+      })
   })
 });
