050d65e1e8c36ff8730ff3cd778c8dd2d42e9462
gumieProf <mine>
2020-07-11 12:52:38 +0900

    FIX scaner.js

--- public/script/scaner.js
+++ public/script/scaner.js
@@ -15,12 +15,8 @@ $(function () {
         var ris = result.data.text;
         console.log(ris);
         var matchrs = ris.match(/[[a-zA-Zあ-ん]:+[0-9a-zA-Z]{9,25}]{2,}/g);
-        matchrs.slice(4);
         $("#loading").text("実行完了！");
         console.log(matchrs);
-        $("#scanData").text(matchrs).select();
-        document.execCommand("copy");
-        process.exit()
       })
     return false;
   })
