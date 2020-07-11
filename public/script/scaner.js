ac09fb9da5a23409d27a026ac3633b71abdd0bc9
gumieProf <mine>
2020-07-11 12:54:51 +0900

    FIX scaner.js

--- public/script/scaner.js
+++ public/script/scaner.js
@@ -3,21 +3,21 @@ $(function () {
   $("#scanFile").change(function () {
 
     var scanImage = $(this)[0].files[0];
-
-    Tesseract.recognize(
-      scanImage,
-      'jpn',
-      {
-        logger: p => $("#loading").text(p.status + ":" + Math.round(p.progress * 100) + "%")
-      }
-    )
-      .then(result => {
-        var ris = result.data.text;
-        console.log(ris);
-        var matchrs = ris.match(/[[a-zA-Zあ-ん]:+[0-9a-zA-Z]{9,25}]{2,}/g);
-        $("#loading").text("実行完了！");
-        console.log(matchrs);
-      })
-    return false;
+    if (!scanImage) {
+      Tesseract.recognize(
+        scanImage,
+        'jpn',
+        {
+          logger: p => $("#loading").text(p.status + ":" + Math.round(p.progress * 100) + "%")
+        }
+      )
+        .then(result => {
+          var ris = result.data.text;
+          console.log(ris);
+          $("#loading").text("実行完了！");
+          $("#scanData").text(ris);
+        })
+      return false;
+    }
   })
 });
