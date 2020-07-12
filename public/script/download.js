7886c16ff9857d9fd4bf1fb2ae558103453aad45
gumieProf <mine>
2020-07-12 15:01:01 +0900

    FIX main dl WPscripts

--- public/script/download.js
+++ public/script/download.js
@@ -137,7 +137,7 @@ export default function download() {
 
           m.fillText(line, x + 0, y + addY);
           var a = new Image();
-          (a.src = "" + pic), m.drawImage(a, 320 * 5, 130 * 5, 50 * 5, 50 * 5);
+          (a.src = "" + pic), m.drawImage(a, prof.height - 30, prof.width - 30, 100, 100);
         }
         let link = document.createElement("a");
         link.href = prof.toDataURL("image/png");
@@ -240,7 +240,7 @@ export default function download() {
 
           m.fillText(line, x + 0, y + addY);
           var a = new Image();
-          (a.src = "" + pic), m.drawImage(a, 320 * 5, 130, 900, 1700);
+          (a.src = "" + pic), m.drawImage(a, prof.height - 30, prof.width - 30, 100, 100);
         }
         let link = document.createElement("a");
         link.href = prof.toDataURL("image/png");
--- public/script/main.js
+++ public/script/main.js
@@ -234,7 +234,7 @@ export default function main() {
 
             m.fillText(line, x + 0, y + addY);
             var a = new Image();
-            (a.src = "" + pic), m.drawImage(a, 320, 130, 50, 50);
+            (a.src = "" + pic), m.drawImage(a, prof.height - 30, prof.width - 30, 50, 50);
           }
         }, 50);
       };
