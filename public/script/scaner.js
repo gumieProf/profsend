ef74fb9cc82190f1cb8a33c128a7c34ad8c5c62b
gumieProf <mine>
2020-07-20 18:18:39 +0900

    FIX pwaTop scaner modules WPmodules,scaner

--- public/script/scaner.js
+++ public/script/scaner.js
@@ -1,5 +1,6 @@
 import Tesseract from 'tesseract.js'
 $(function () {
+  var userAgent = window.navigator.userAgent;
   $("#scanFile").change(function (e) {
 
     var scanImage = $(this)[0].files[0];
@@ -19,9 +20,22 @@ $(function () {
         .then(result => {
           var ris = result.data.text;
           console.log(ris);
-
+          var rismatch = ris.match(/[a-zA-Z]: [a-zA-Z0-9あ-ん]/g);
+          console.log(rismatch);
+          if (version.indexOf("msie 8.") != -1) {
+            function isArray(obj) {
+              return Object.prototype.toString.call(obj) === "[object Array]"
+            }
+          }
+          if (rismatch.isArray([])) {
+            for (let i = 0; index < rismatch.length; i++) {
+              var risnam = rismatch[i].match(/[a-zA-Z]:/);
+              var risval = rismatch[i].match(/[A-Za-z]/);
+              $("#scanData > ul").html("<li><strong>" + risnam + "</strong></li>");
+              $("#scanData > ul").html("<li>" + risval + "</li>");
+            }
+          }
           $("#loading").text("実行完了！");
-          $("#scanData").html(ris);
         })
       return false;
     }
