d2a7d06589dc7cfe7578b74ca967c9b32715ff17
gumieProf <gutsmine910@gmail.com>
2021-07-27 12:49:55 +0900

    ベースシステムの改善（cssフレームワークのインポート方法の変更など）

--- public/script/modules.js
+++ public/script/modules.js
@@ -1,8 +1,7 @@
-import css from "uikit/dist/css/uikit.css";
+import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
-import UIkit from "uikit/dist/js/uikit.js";
-import jquery from "jquery";
 import "../style.css";
+import jquery from "jquery";
 UIkit.use(Icons);
 window.UIkit = UIkit;
 window.$ = jquery;
