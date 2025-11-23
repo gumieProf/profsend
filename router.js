c98513f520747bc68be0118996ba28caa43242b0
gumieProf <gutsmine910@gmail.com>
2025-11-23 09:36:04 +0900

    FIX gitignore DEL csurf RM builded scripts

--- router.js
+++ router.js
@@ -1,38 +1,36 @@
 ﻿var router = require("express").Router();
-var csrf = require("csurf");
 var fs = require("fs");
-var csrfProtection = csrf({ cookie: false });
 
-router.get("/", csrfProtection, function (req, res) {
+router.get("/", function (req, res) {
   res.sendFile("welcome.html", { root: __dirname + "/public/" });
 });
-router.get("/top", csrfProtection, function (req, res) {
+router.get("/top", function (req, res) {
   res.sendFile("welcome.html", { root: __dirname + "/public/" });
 });
 
-router.get("/gaming.html", csrfProtection, function (req, res) {
+router.get("/gaming.html", function (req, res) {
   res.sendFile("gaming.html", { root: __dirname + "/public/" });
 });
 
-router.get("/about.html", csrfProtection, function (req, res) {
+router.get("/about.html", function (req, res) {
   res.sendFile("about.html", { root: __dirname + "/public/" });
 });
 
-router.get("/gaming", csrfProtection, function (req, res) {
+router.get("/gaming", function (req, res) {
   res.sendFile("gaming.html", { root: __dirname + "/public/" });
 });
 
-router.get("/about", csrfProtection, function (req, res) {
+router.get("/about", function (req, res) {
   res.sendFile("about.html", { root: __dirname + "/public/" });
 });
 
-router.get("/13update", csrfProtection, function (req, res) {
+router.get("/13update", function (req, res) {
   res.sendFile("13update.html", { root: __dirname + "/public/" });
 });
-router.get("/13update.html", csrfProtection, function (req, res) {
+router.get("/13update.html", function (req, res) {
   res.sendFile("13update.html", { root: __dirname + "/public/" });
 });
-router.get("/business", csrfProtection, function (req, res) {
+router.get("/business", function (req, res) {
   res.sendFile("business.html", { root: __dirname + "/public/" });
 });
 
